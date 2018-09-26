a6c1d8e4ea5c823c310290b738d742176ddc1dc4
Mike <mike.a.zhigalov@gmail.com>
2018-09-27 00:48:12 +0300

    Added stubs for hello and hi controllers. Added general logic for model.

---
+++ TestMyCredit/Models/HiGreating.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestMyCredit.Models
+{
+    public class HiGreating : IGreeter
+    {
+        public string SayHello()
+        {
+            return "Hi everyone!";
+        }
+    }
+}
---
+++ TestMyCredit/Models/IGreeter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyCredit.Models
+{
+    public interface IGreeter
+    {
+        string SayHello();
+    }
+}
