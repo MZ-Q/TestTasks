002ec07ee5483a8143830cd0d7f222103302c5dc
MZ-Q <mike.a.zhigalov@gmail.com>
2018-09-27 05:57:59 +0300

    Changed lifetime mgr for the IGreater objects. Added comments for the delicate moment.

--- TestMyCredit/App_Start/WebApiConfig.cs
+++ TestMyCredit/App_Start/WebApiConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web.Http;
 using Unity;
 using Unity.Injection;
+using Unity.Lifetime;
 
 namespace TestMyCredit
 {
@@ -13,10 +14,17 @@ namespace TestMyCredit
         {
             // Web API configuration and services
             var container = new UnityContainer();
-            container.RegisterType<Models.IGreeter, Models.HiGreating>();
-            container.RegisterType<Controllers.HiController>(new InjectionConstructor(container.Resolve<Models.IGreeter>()));
-            container.RegisterType<Models.IGreeter, Models.HelloGreating>();
+
+            // No need to recreate IGreeter types, because they are immutable in current implementation
+            container.RegisterType<Models.IGreeter, Models.HiGreating>(new ContainerControlledLifetimeManager());
+
+            // Use default(TransientLifetimeManager) lifetime mgr because of re-construction Conrollers on update
+            container.RegisterType<Controllers.HiController>(new ContainerControlledLifetimeManager(), new InjectionConstructor(container.Resolve<Models.IGreeter>()));
+
+            // Same logic for the "Hello" controller
+            container.RegisterType<Models.IGreeter, Models.HelloGreating>(new ContainerControlledLifetimeManager());
             container.RegisterType<Controllers.HelloController>(new InjectionConstructor(container.Resolve<Models.IGreeter>()));
+
             config.DependencyResolver = new Resolver.UnityResolver(container);
 
             // Web API routes
